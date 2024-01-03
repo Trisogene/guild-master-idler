449385e866cfe59978c6d8b8be4ae101eeaa8904
trisogene <trisogene@gmail.com>
2024-01-04 00:51:08 +0100

    Refactor recruit and reward log components

--- src/components/Log/RecruitLog.tsx
+++ src/components/Log/RecruitLog.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, Divider, Typography } from "@mui/joy";
+import { Card, Chip, Typography } from "@mui/joy";
 import { T_Log_Recruit } from "../../config/store.d";
 
 interface I_RecruitLogProps {
@@ -6,18 +6,32 @@ interface I_RecruitLogProps {
 }
 const RecruitLog = ({ log }: I_RecruitLogProps) => {
   return (
-    <Card sx={{ gap: 0.5, p: 0.5 }}>
-      <Box sx={{ display: "flex", gap: 1 }}>
-        <Typography fontSize={12} sx={{ minWidth: 35 }}>
+    <Card
+      sx={{
+        p: 1,
+        minHeight: 38,
+        flexDirection: "row",
+        gap: 1,
+        alignItems: "center",
+      }}
+    >
+      <Chip variant="outlined">
+        <Typography
+          fontSize={12}
+          sx={{
+            minWidth: 35,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           {new Date(log.timestamp * 1000).toISOString().substr(14, 5)}
-        </Typography>
+        </Typography>{" "}
+      </Chip>
 
-        <Divider orientation="vertical" sx={{ height: "100%" }} />
-
-        <Typography fontSize={12} sx={{ minWidth: 80 }}>
-          {log.playerName} Recruited
-        </Typography>
-      </Box>
+      <Typography fontSize={10} sx={{ minWidth: 80 }}>
+        {log.playerName} Recruited
+      </Typography>
     </Card>
   );
 };
--- src/components/Log/RewardLog.tsx
+++ src/components/Log/RewardLog.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Card, Chip, Tooltip, Typography } from "@mui/joy";
+import { Avatar, Card, Chip, Tooltip, Typography } from "@mui/joy";
 import { CONTENTS, RACES } from "../../config/config";
 import { T_Log_Reward } from "../../config/store.d";
 import Item from "../Item/Item";
@@ -8,43 +8,36 @@ interface I_RewardLogProps {
 }
 const RewardLog = ({ log }: I_RewardLogProps) => {
   return (
-    <Card sx={{ p: 1 }}>
-      <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
-        <Chip variant="outlined">
-          <Typography
-            fontSize={12}
-            sx={{
-              minWidth: 35,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            {new Date(log.timestamp * 1000).toISOString().substr(14, 5)}
-          </Typography>
-        </Chip>
-
-        <Tooltip
-          disableInteractive
-          arrow
-          title={log.playerName}
-          placement="top"
+    <Card sx={{ p: 1, flexDirection: "row", gap: 1, alignItems: "center" }}>
+      <Chip variant="outlined">
+        <Typography
+          fontSize={12}
+          sx={{
+            minWidth: 35,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         >
-          <Avatar
-            src={RACES[log.playerRace]?.img}
-            sx={{ width: 16, height: 16 }}
-          />
-        </Tooltip>
+          {new Date(log.timestamp * 1000).toISOString().substr(14, 5)}
+        </Typography>
+      </Chip>
 
-        <Tooltip disableInteractive arrow title={log.contentId} placement="top">
-          <Avatar
-            src={CONTENTS[log.contentId].icon}
-            sx={{ width: 16, height: 16 }}
-          />
-        </Tooltip>
+      <Tooltip disableInteractive arrow title={log.playerName} placement="top">
+        <Avatar
+          src={RACES[log.playerRace]?.img}
+          sx={{ width: 16, height: 16 }}
+        />
+      </Tooltip>
 
-        <Item itemStack={log.reward} size="sm" />
-      </Box>
+      <Tooltip disableInteractive arrow title={log.contentId} placement="top">
+        <Avatar
+          src={CONTENTS[log.contentId].icon}
+          sx={{ width: 16, height: 16 }}
+        />
+      </Tooltip>
+
+      <Item itemStack={log.reward} size="sm" />
     </Card>
   );
 };
